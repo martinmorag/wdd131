773b402ab7ab6c678f1851808f45b3c9889f2013
Martin Moraga <moragamartin2@gmail.com>
2026-05-14 11:10:42 -0300

    place project

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,3 +2,8 @@ const today = new Date()
 document.getElementById("currentYear").innerHTML = today.getFullYear()
 
 document.getElementById("lastModified").innerHTML = document.lastModified;
+
+let names = ['Nancy', 'Blessing', 'Jorge', 'Svetlana', 'Bob'];
+
+const newarr = names.reduce((acc, curr) => acc / names.length)
+console.log(newarr)

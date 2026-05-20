69fe96ca014725cf0a02220c911a906d14012698
Martin Moraga <moragamartin2@gmail.com>
2026-05-20 10:25:48 -0300

    form assignment

---
+++ scripts/form.js
@@ -0,0 +1,43 @@
+const today = new Date()
+document.getElementById("currentYear").innerHTML = today.getFullYear()
+
+document.getElementById("lastModified").innerHTML = document.lastModified;
+
+
+const products = [
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
+    }
+];
+
+const select = document.getElementById('products')
+
+products.forEach(prod => {
+    let op = document.createElement('option');
+    op.value = prod.id;
+    op.textContent = prod.name;
+
+    select.append(op)
+})
--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,8 +2,3 @@ const today = new Date()
 document.getElementById("currentYear").innerHTML = today.getFullYear()
 
 document.getElementById("lastModified").innerHTML = document.lastModified;
-
-let names = ['Nancy', 'Blessing', 'Jorge', 'Svetlana', 'Bob'];
-
-const newarr = names.reduce((acc, curr) => acc / names.length)
-console.log(newarr)
